d291044aaafdea58dc29bcd269f7a63c2150d9f3
Raffaele Marcello <marcelloraffaele@gmail.com>
2019-10-10 20:28:18 +0200

    project creation

---
+++ src/test/java/it/rmarcello/appenginespringboot/AppengineSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package it.rmarcello.appenginespringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppengineSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
